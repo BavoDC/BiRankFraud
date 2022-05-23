a985e464ddd7e22b606dfe022897d4c02e941298
BavoDC <BavoDC@users.noreply.github.com>
2022-05-23 15:09:47 +0200

    Add files package

---
+++ R/printFunctions.R
@@ -0,0 +1,12 @@
+#' Print method for object of type BiRankFr
+#'
+#' @param x Object of type BiRankFr
+#' @param ... Ellipses argument, to be passed to \code{print} function.
+#'
+#' @return Nothing
+print.BiRankFr <- function(x, ...) {
+  cat("\n\nResults claims\n\n")
+  print(head(x$ResultsClaims), ...)
+  cat("\n\nResults parties\n\n")
+  print(head(x$ResultsParties), ...)
+}
